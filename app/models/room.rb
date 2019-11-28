299223841c14cd6418310b01bbfe463cc2af7d0d
root <root@LAPTOP-8DE7N6T3.localdomain>
2019-11-28 16:00:35 -0500

    mejorando validaciones

--- app/models/room.rb
+++ app/models/room.rb
@@ -15,8 +15,8 @@
 class Room < ActiveRecord::Base
     validates :title, presence: true
     validates :description, presence: true, length: { in: 0..400 }
-    validates :beds, presence: true
-    validates :guests, presence: true
+    validates :beds, presence: true, numericality: { only_integer: true }
+    validates :guests, presence: true, numericality: { only_integer: true }
     validates :image_url, presence: true
     validates :description, presence: true
 end
