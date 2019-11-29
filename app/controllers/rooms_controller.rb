37206a9ce3d684175dda4b1c3e01aca42290e9f8
root <root@LAPTOP-8DE7N6T3.localdomain>
2019-11-28 20:49:37 -0500

    funcionalidad editar o eliminar registros

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -16,8 +16,29 @@ class RoomsController < ApplicationController
     end
   end
 
+  def edit
+    @room = Room.find(params[:id])
+  end
+  
+  def update
+    @room = Room.find(params[:id])
+    if @room.update(room_params)
+      redirect_to rooms_path, notice: "La habitación ha sido modificada con éxito"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    room = Room.find(params[:id])
+    room.destroy
+  
+    redirect_to rooms_path, notice: "El producto fue eliminado con éxito"
+  end
+
   protected
     def room_params
       params.require(:room).permit(:title, :description, :beds, :guests, :image_url, :price_per_night)
     end
+    
 end
